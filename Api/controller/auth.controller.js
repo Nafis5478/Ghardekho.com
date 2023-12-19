e08dafcdce13da41227e334106bd839af9b96269
Nafis5478 <mohammad.nafis.raza.28@gmail.com>
2023-12-19 19:56:36 +0530

    Created a middler and a function errorHandler to handle all kind of errors in api

--- Api/controller/auth.controller.js
+++ Api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-export const signup = async (req, res) => {
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res,next) => {
     const { username, email, password } = req.body;
     const salt = await bcryptjs.genSalt(10);
     const hashedpassword = await bcryptjs.hash(password, salt);
@@ -11,6 +12,6 @@ export const signup = async (req, res) => {
         res.status(201).json({ message: 'New user created successfully.' });
     } catch (error) {
         // Handle the error, for example, send an error response
-        res.status(500).json({ error: 'Internal Server Error' });
+        next(error)
     }
 };
