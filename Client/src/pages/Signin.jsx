b88203c0bcdda5bca61f7dc19d6708d17e17c3e5
Nafis5478 <mohammad.nafis.raza.28@gmail.com>
2023-12-21 03:45:51 +0530

    added redux persist to store the logged in user and prevent them from begin logged out while refreshing the page.

--- Client/src/pages/Signin.jsx
+++ Client/src/pages/Signin.jsx
@@ -38,7 +38,7 @@ export default function Signin() {
   };
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           type='text'
@@ -59,7 +59,7 @@ export default function Signin() {
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {loading ? 'Loading...' : 'Sign Ip'}
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
