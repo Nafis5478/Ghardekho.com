09fa22192d45ff81caefd38f0ebdd1630e9f2b2c
Nafis5478 <mohammad.nafis.raza.28@gmail.com>
2023-12-23 01:43:12 +0530

    Complete image upload functionality in profile.js

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -1,41 +1,99 @@
-import { current } from '@reduxjs/toolkit';
-import React from 'react'
-import { useSelector } from 'react-redux';
+import { current } from "@reduxjs/toolkit";
+import React, { useEffect, useState } from "react";
+import { useRef } from "react";
+import { useSelector } from "react-redux";
+import {
+  getStorage,
+  uploadBytes,
+  uploadBytesResumable,
+  ref,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
 export default function Profile() {
-  const {currentUser}=useSelector((state)=>state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name; // we add timestamp in file name so that we get unique file everytime.
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // setFilePerc(progress)
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, avatar: downloadURL });
+        });
+      }
+    );
+  };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-center my-5 text-3xl font-semibold'>
-        Profile
-      </h1>
-      <form className='flex flex-col gap-4' >
-        <img src={currentUser.avatar} alt={'profile'} className='rounded-full h-30 w-30 object-cover cursor-pointer self-center mt-2'/>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-center my-5 text-3xl font-semibold">Profile</h1>
+      <form className="flex flex-col gap-4">
         <input
-          type='text'
-          placeholder='Username'
-          className='border p-3 rounded-lg'
-          id='username'
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar||currentUser.avatar}
+          alt={"profile"}
+          className="rounded-full object-cover cursor-pointer self-center mt-2 h-24 w-24"
+        />
+        <p className="text-sm self-center">
+          {
+            fileUploadError?(<span className="text-red-700">Error Image Upload (image size must be less than 2MB)</span>):
+            filePerc>0&&filePerc<100 ? (<span className="text-slate-700">
+              {`Uploading ${filePerc}%`}
+            </span>):
+            filePerc===100?(<span className="text-green-700">Uploaded successfully</span>):
+            ""
+          }
+        </p>
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg"
+          id="username"
         />
         <input
-          type='email'
-          placeholder='Email'
-          className='border p-3 rounded-lg'
-          id='email'
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg"
+          id="email"
         />
         <input
-          type='text'
-          placeholder='Password'
-          className='border p-3 rounded-lg'
-          id='password'
+          type="text"
+          placeholder="Password"
+          className="border p-3 rounded-lg"
+          id="password"
         />
-        <button
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-        >
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           update
         </button>
-        <button
-          className='bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-        >
+        <button className="bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
           add new list
         </button>
       </form>
@@ -44,5 +102,5 @@ export default function Profile() {
         <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
     </div>
-  )
+  );
 }
