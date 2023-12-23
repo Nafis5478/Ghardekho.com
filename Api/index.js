9d6e010200cac2d62022cfdc869e8a8c8bc3ffc3
Nafis5478 <mohammad.nafis.raza.28@gmail.com>
2023-12-23 17:26:43 +0530

    Created update user api

--- Api/index.js
+++ Api/index.js
@@ -1,25 +1,3 @@
-// import express from  'express'
-// import mongoose from 'mongoose'
-// import dotenv from 'dotenv'
-// import userRoute from './route/user.router.js'
-// import authRoute from './route/auth.router.js'
-// dotenv.config()
-// mongoose.connect(process.env.MONGO)
-// .then(()=>{
-//     console.log('connect to database...');
-// })
-// .catch((err)=>{
-//     console.log(err);
-// });
-
-// const app=express();
-
-// app.listen(3000,()=>{
-//     console.log('Server is running on port 3000!!!');
-// });
-// app.use(express.json())
-// app.use("/api/user",userRoute)
-// app.use("/api/auth",authRoute)
 
 
 // while using mongo db compass...
@@ -29,12 +7,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoute from './route/user.router.js';
 import authRoute from './route/auth.router.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
-
+// in case we are using mongo db compass....
 mongoose.connect(process.env.MONGO2, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     dbName: process.env.dbname // Specify the database name
 })
     .then(() => {
@@ -43,14 +20,25 @@ mongoose.connect(process.env.MONGO2, {
     .catch((err) => {
         console.error('Error connecting to the database:', err);
     });
+// in case we are using mongo db atlas....
+// mongoose.connect(process.env.MONGO)
+//     .then(() => {
+//         console.log('Connected to the database...');
+//     })
+//     .catch((err) => {
+//         console.error('Error connecting to the database:', err);
+//     });
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!!!');
+    console.log('Server is running on port 3000');
 });
 
 app.use(express.json());
+app.use(cookieParser());
+
+
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 app.use((err,req,res,next)=>{
--- Api/route/user.router.js
+++ Api/route/user.router.js
@@ -1,6 +1,8 @@
 import express from "express";
-import {test} from '../controller/user.controller.js'
+import {test,update} from '../controller/user.controller.js'
+import {verifyUser} from '../utils/verifyUser.js'
 const router= express.Router();
 
 router.get('/test',test)
+router.put('/update/:id',verifyUser,update)
 export default router
